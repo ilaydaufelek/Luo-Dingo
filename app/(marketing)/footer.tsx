dd90a56a86a735381baf78d5483751e0d19d5390
ilaydaufelek <ilaydaufelek@gmail.com>
2026-03-10 14:18:29 +0300

    feat: create footer with language buttons

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,10 +1,43 @@
- export const Footer = () => {
-    return (
-      <div>
-        <footer className="hidden lg:block h-20 w-full border-t-2 border-slate p-2" >
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
-        </footer>
-      </div>  
-      );
-}
- 
+export const Footer = () => {
+  return (
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      <div className="max-w-5xl mx-auto flex items-center gap-2 h-full">
+        <Button size="lg" variant="ghost" className="flex-1">
+          <Image
+            className="mr-4 rounded-md"
+            width={40}
+            height={32}
+            src="/tr.svg"
+            alt="Turkish"
+          />
+          Turkish
+        </Button>
+
+        <Button size="lg" variant="ghost" className="flex-1">
+          <Image
+            className="mr-4 rounded-md"
+            width={40}
+            height={32}
+            src="/it.svg"
+            alt="Italian"
+          />
+          Italian
+        </Button>
+
+        <Button size="lg" variant="ghost" className="flex-1">
+          <Image
+            className="mr-4 rounded-md"
+            width={40}
+            height={32}
+            src="/jp.svg"
+            alt="Japanese"
+          />
+          Japanese
+        </Button>
+      </div>
+    </footer>
+  );
+};
